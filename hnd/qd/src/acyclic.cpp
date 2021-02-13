4180825a85f90e5edd4d36f79f65ecfdfc4fd6c7
Erdem <a.erdemsariyuce@gmail.com>
2021-02-13 07:31:14 -0800

    acyclic

--- hnd/qd/src/acyclic.cpp
+++ hnd/qd/src/acyclic.cpp
@@ -24,8 +24,6 @@ double simple_count_acyclics (Graph& dgraph, unordered_map<int, bool>& numbers, 
 				if (crossing.find(w) != crossing.end())
 					asdf++;
 
-//				printf ("u: %d v: %d w: %d -- asdf: %d\n", u, v, w, asdf);
-
 				if (asdf < 3) {
 					if (numbers.find(u) != numbers.end())
 						vol++;
@@ -58,8 +56,6 @@ double simple_count_acyclics (Graph& dgraph, unordered_map<int, bool>& numbers, 
 						asdf++;
 					if (crossing.find(w) != crossing.end())
 						asdf++;
-
-	//				printf ("u: %d v: %d w: %d -- asdf: %d\n", u, v, w, asdf);
 
 					if (asdf < 3) {
 						if (numbers.find(u) != numbers.end())
@@ -108,22 +104,6 @@ vertex count_acyclics (Graph& dgraph, Graph& TC) {
 				TC[v][ints[k+1]]++; // v-w
 				count++;
 				printf ("acyclic: %d %d %d\n", u, v, w);
-#ifdef ORBITS
-				vector<vertex> a = {u, v};
-				sort (a.begin(), a.end());
-				auto t = make_tuple (a[0], a[1]);
-				orb1[t]++;
-
-				a = {v, w};
-				sort (a.begin(), a.end());
-				t = make_tuple (a[0], a[1]);
-				orb2[t]++;
-
-				a = {u, w};
-				sort (a.begin(), a.end());
-				t = make_tuple (a[0], a[1]);
-				orb3[t]++;
-#endif
 #else
 				_m11 = _11 = s1 = signs[u][ret[r]]; // u-v
 				_m12 = _12 = s2 = signs[u][ints[k]]; // u-w
@@ -141,14 +121,6 @@ vertex count_acyclics (Graph& dgraph, Graph& TC) {
 					TC[u][ints[k]]++; // u-w
 					TC[v][ints[k+1]]++; // v-w
 					count++;
-//					vector<vertex> a = {u, v, w};
-//					sort (a.begin(), a.end());
-//					auto t = make_tuple (a[0], a[1], a[2]);
-//
-//					if (mp.find (t) == mp.end()) {
-//						mp[t] = true;
-//						printf ("acyclic: %d %d\t%d %d\t%d %d\t %d %d %d\n", u, v, v, w, w, u, signs[u][ret[r]], signs[u][ints[k]], signs[v][ints[k+1]]);
-//					}
 				}
 
 #endif
@@ -157,110 +129,10 @@ vertex count_acyclics (Graph& dgraph, Graph& TC) {
 		}
 	}
 	printf ("total acyclic count: %d\n", count);
-
-#ifdef ORBITS
-	for (auto it = orb1.begin(); it != orb1.end(); it++)
-		printf ("orb1 of %d %d is %d\n", get<0>(it->first), get<1>(it->first), it->second);
-
-	for (auto it = orb2.begin(); it != orb2.end(); it++)
-		printf ("orb2 of %d %d is %d\n", get<0>(it->first), get<1>(it->first), it->second);
-
-	for (auto it = orb3.begin(); it != orb3.end(); it++)
-		printf ("orb3 of %d %d is %d\n", get<0>(it->first), get<1>(it->first), it->second);
-
-	exit(1);
-#endif
-
-
 	return count;
 }
 
-//
-//void acyclic_truss (Graph& graph, bool hierarchy, edge nEdge, vector<vertex>& K, vertex* maxK, FILE* fp) {
-//	const auto t1 = chrono::steady_clock::now();
-//	// Acyclic counting for each edge
-//	vertex nVtx = graph.size();
-//	Graph TC;
-//	TC.resize(nVtx);
-//	for (vertex i = 0; i < nVtx; i++)
-//		TC[i].resize (graph[i].size(), 0);
-//	lol tric =count_acyclics (graph, TC);
-//	fprintf (fp, "# acyclis: %lld\n", tric);
-//	const auto t2 = chrono::steady_clock::now();
-//	print_time (fp, "Acyclic counting: ", t2 - t1);
-//
-//	// Peeling
-//	const auto p1 = chrono::steady_clock::now();
-//	K.resize (nEdge, -1);
-//	Naive_Bucket nBucket;
-//	nBucket.Initialize (nVtx, nEdge);
-//	vertex id = 0;
-//	vector<vp> el;
-//	vector<vertex> xel;
-//	xel.push_back(0);
-//	// each non-reciprocal edge and its acyclic-count is inserted to bucket
-//	for (vertex i = 0; i < graph.size(); i++) {
-//		vector<vertex> ret;
-//		outgoings (graph[i], ret);
-//		for (vertex r = 0; r < ret.size(); r++) {
-//			vp c (i, graph[i][ret[r]]);
-//			el.push_back(c); // first is source, second is target
-//			printf ("acyclic count of %d is %d\n", id, TC[i][ret[r]]);
-//			if (TC[i][ret[r]] > 0)
-//				nBucket.Insert (id++, TC[i][ret[r]]);
-//			else
-//				K[id++]= 0;
-//		}
-//		xel.push_back(el.size());
-//	}
-//	vertex tc_e = 0;
-//	while (true) {
-//		edge e;
-//		vertex val;
-//		if (nBucket.PopMin(&e, &val) == -1) // if the bucket is empty
-//			break;
-//		tc_e = K[e] = val;
-//		vertex u = el[e].first; // source
-//		vertex v = el[e].second; // target
-//		vector<vertex> ints;
-//		inter (2, 2, graph, u, v, ints); // blue orbit
-//		inter (1, 1, graph, u, v, ints); // green orbit
-//		inter (2, 1, graph, u, v, ints); // purple orbit
-//		vertex id1, id2;
-//		for (auto k = 0; k < ints.size(); k+=2) {
-//			vertex w = graph[u][ints[k]];
-//			vertex x = graph[v][ints[k+1]];
-//			if (w >= 0 && x >= 0) { // blue orbit
-//				id1 = getEdgeId (u, w, xel, el, graph);
-//				id2 = getEdgeId (v, w, xel, el, graph);
-//			}
-//			else if (w < 0 && x < 0) { // green orbit
-//				w = M2P (w);
-//				id1 = getEdgeId (w, u, xel, el, graph);
-//				id2 = getEdgeId (w, v, xel, el, graph);
-//			}
-//			else if (w >= 0 && x < 0) { // purple orbit
-//				id1 = getEdgeId (u, w, xel, el, graph);
-//				id2 = getEdgeId (w, v, xel, el, graph);
-//			}
-//			checkAndDec (K[id1], K[id2], id1, id2, &nBucket, tc_e);
-//		}
-//	}
-//	nBucket.Free();
-//	*maxK = tc_e;
-//	const auto p2 = chrono::steady_clock::now();
-//
-//	if (!hierarchy) {
-//		print_time (fp, "Only peeling time: ", p2 - p1);
-//		print_time (fp, "Total time: ", (p2 - p1) + (t2 - t1));
-//	}
-//	for (auto i = 0; i < el.size(); i++)
-//		printf ("truss of %d (%d-%d) is %d\n", i, el[i].first, abs(el[i].second), K[i]); // the ones with -1 kappa either do not participate in any outp or u > v for the corresponding u-v edge
-//	return;
-//}
-
-
-void acyclic_truss_SUBS (Graph& graph, bool hierarchy, edge nEdge, vector<vertex>& K, vertex* maxK, FILE* fp, string vfile) {
+void acyclic_truss (Graph& graph, bool hierarchy, edge nEdge, vector<vertex>& K, vertex* maxK, FILE* fp, string vfile) {
 	const auto t1 = chrono::steady_clock::now();
 	// Acyclic counting for each edge
 	vertex nVtx = graph.size();
@@ -296,7 +168,6 @@ void acyclic_truss_SUBS (Graph& graph, bool hierarchy, edge nEdge, vector<vertex
 		for (vertex r = 0; r < ret.size(); r++) {
 			vp c (i, graph[i][ret[r]]);
 			el.push_back(c); // first is source, second is target
-//			printf ("acyclic count of %d is %d\n", id, TC[i][ret[r]]);
 			if (TC[i][ret[r]] > 0) {
 				nBucket.Insert (id++, TC[i][ret[r]]);
 				dist[TC[i][ret[r]]]++;
@@ -387,7 +258,7 @@ void acyclic_truss_SUBS (Graph& graph, bool hierarchy, edge nEdge, vector<vertex
 #endif
 
 			}
-			//			checkAndDec (K[id1], K[id2], id1, id2, &nBucket, tc_e);
+
 #ifndef SIGNS
 			checkAndDecAndHier (id2, id1, &nBucket, tc_e, e, hierarchy, &nSubcores, K, skeleton, component, unassigned, relations);
 #else
@@ -437,36 +308,11 @@ void acyclic_truss_SUBS (Graph& graph, bool hierarchy, edge nEdge, vector<vertex
 		print_time (fp, "Total acyclic-truss nucleus decomposition time: ", (p2 - p1) + (t2 - t1) + (b2 - b1) + (d2 - d1));
 	}
 
-	for (auto i = 0; i < el.size(); i++)
-		printf ("listfor %d ( %d - %d ): K: %d\n", i, el[i].first, abs(el[i].second), K[i]); // the ones with -1 kappa either do not participate in any outp or u > v for the corresponding u-v edge
-
-
-//	for (auto i = 0; i < el.size(); i++)
-//		printf ("truss of %d (%d-%d) is %d\n", i, el[i].first, abs(el[i].second), K[i]); // the ones with -1 kappa either do not participate in any outp or u > v for the corresponding u-v edge
 	return;
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// vertex count_acyclics (Graph& dgraph, Graph& TC) {
 vertex count_acyclics_roleAware (Graph& dgraph, Graph& TC1, Graph& TC2, Graph& TC3) {
 	vertex count = 0;
 	for (vertex u = 0; u < dgraph.size(); u++) {
@@ -482,18 +328,14 @@ vertex count_acyclics_roleAware (Graph& dgraph, Graph& TC1, Graph& TC2, Graph& T
 				TC2[u][ints[k]]++; // u-w
 				TC3[v][ints[k+1]]++; // v-w
 				count++;
-				printf ("TC-1: %d %d\n", u, v);
-				printf ("TC-2: %d %d\n", u, w);
-				printf ("TC-3: %d %d\n", v, w);
 			}
 		}
 	}
 	printf ("total acyclic count: %d\n", count);
-	exit(1);
 	return count;
 }
 
-void acyclic_truss_SUBS_roleAware (Graph& graph, bool hierarchy, edge nEdge, vector<vertex>& K, vertex* maxK, FILE* fp, string vfile) {
+void acyclic_truss_roleAware (Graph& graph, bool hierarchy, edge nEdge, vector<vertex>& K, vertex* maxK, FILE* fp, string vfile) {
 	const auto t1 = chrono::steady_clock::now();
 	// Acyclic counting for each edge
 	vertex nVtx = graph.size();
@@ -537,7 +379,6 @@ void acyclic_truss_SUBS_roleAware (Graph& graph, bool hierarchy, edge nEdge, vec
 		for (vertex r = 0; r < ret.size(); r++) {
 			vp c (i, graph[i][ret[r]]);
 			el.push_back(c); // first is source, second is target
-//          printf ("acyclic count of %d is %d\n", id, TC[i][ret[r]]);
 			if (TC1[i][ret[r]] > 0) {
 				nBucket.Insert (id, TC1[i][ret[r]]);
 			}
@@ -794,133 +635,6 @@ void acyclic_truss_SUBS_roleAware (Graph& graph, bool hierarchy, edge nEdge, vec
 		}
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// edge traversal
-	if (0) {
-		vector<bool> visited_1 (nEdge, false);
-		vector<bool> visited_2 (nEdge, false);
-		vector<bool> visited_3 (nEdge, false);
-
-		for (auto i = 0; i < el.size(); i++) {
-			queue<vp> q;
-			if (K1[i] == *maxK && !visited_1[i]) {
-				printf ("start of loop\n");
-				q.push(make_pair (i, 1));
-				visited_1[i] = true;
-			}
-			if (K2[i] == *maxK && !visited_2[i]) {
-				printf ("start of loop\n");
-				q.push(make_pair (i, 2));
-				visited_2[i] = true;
-			}
-			if (K3[i] == *maxK && !visited_3[i]) {
-				printf ("start of loop\n");
-				q.push(make_pair (i, 3));
-				visited_3[i] = true;
-			}
-			while (!q.empty()) {
-				auto c = q.front();
-				q.pop();
-				int e = c.first;
-				int type = c.second;
-				vertex u = el[e].first; // source
-				vertex v = el[e].second; // target
-				//			printf ("Process: %d %d\n", u, v);
-
-				vector<vertex> ints;
-				if (type == 1) {
-					printf ("T-1: %d %d\n", u, v);
-					inter (2, 2, graph, u, v, ints); // u-v is type TC1
-				}
-				else if (type == 2) {
-					printf ("T-2: %d %d\n", u, v);
-					inter (2, 1, graph, u, v, ints); // u-v is type TC2
-				}
-				else if (type == 3) {
-					printf ("T-3: %d %d\n", u, v);
-					inter (1, 1, graph, u, v, ints); // u-v is type TC3
-				}
-
-				vertex id1, id2;
-
-				for (auto k = 0; k < ints.size(); k+=2) {
-					vertex w = graph[u][ints[k]];
-					vertex x = graph[v][ints[k+1]];
-					if (w >= 0 && x >= 0) { // blue orbit - TC1
-						id1 = getEdgeId (u, w, xel, el, graph); // 2
-						id2 = getEdgeId (v, w, xel, el, graph); // 3
-						//					printf ("%d %d found: %d %d %d\n", u, w, K1[id1], K2[id1], K3[id1]);
-						//					printf ("%d %d found: %d %d %d\n", v, w, K1[id2], K2[id2], K3[id2]);
-						if (K2[id1] == *maxK && K3[id2] == *maxK) {
-							if (	!visited_2[id1]) {
-								//							printf ("%d %d visited\n", u, w);
-								visited_2[id1] = true;
-								q.push(make_pair (id1, 2));
-							}
-							if (!visited_3[id2]) {
-								//							printf ("%d %d visited\n", v, w);
-								visited_3[id2] = true;
-								q.push(make_pair (id2, 3));
-							}
-						}
-					}
-					else if (w < 0 && x < 0) { // green orbit - TC 3
-						w = M2P (w);
-						id1 = getEdgeId (w, u, xel, el, graph); // 1
-						id2 = getEdgeId (w, v, xel, el, graph); // 2
-						//					printf ("%d %d found: %d %d %d\n", w, u, K1[id1], K2[id1], K3[id1]);
-						//					printf ("%d %d found: %d %d %d\n", w, v, K1[id2], K2[id2], K3[id2]);
-						if (K1[id1] == *maxK && K2[id2] == *maxK) {
-							if (!visited_1[id1]) {
-								//							printf ("%d %d visited\n", w, u);
-								visited_1[id1] = true;
-								q.push(make_pair (id1, 1));
-							}
-							if (!visited_2[id2]) {
-								//							printf ("%d %d visited\n", w, v);
-								visited_2[id2] = true;
-								q.push(make_pair (id2, 2));
-							}
-
-						}
-					}
-					else if (w >= 0 && x < 0) { // purple orbit - TC 2
-						id1 = getEdgeId (u, w, xel, el, graph);
-						id2 = getEdgeId (w, v, xel, el, graph);
-						//					printf ("%d %d found: %d %d %d\n", u, w, K1[id1], K2[id1], K3[id1]);
-						//					printf ("%d %d found: %d %d %d\n", w, v, K1[id2], K2[id2], K3[id2]);
-						if (K1[id1] == *maxK && K3[id2] == *maxK) {
-							if (!visited_1[id1]) {
-								//							printf ("%d %d visited\n", u, w);
-								visited_1[id1] = true;
-								q.push(make_pair (id1, 1));
-							}
-							if (!visited_3[id2]) {
-								//							printf ("%d %d visited\n", w, v);
-								visited_3[id2] = true;
-								q.push(make_pair (id2, 3));
-							}
-						}
-					}
-				}
-			}
-
-		}
-	}
 	return;
 }
 
